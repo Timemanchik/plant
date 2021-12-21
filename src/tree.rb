1ebb9e9cbec420f0dad6ca994ec186b2a4a23e1b
Timemanchik <timemanchik@mail.ru>
2021-12-21 20:48:00 +0300

    plants colorizes 2

--- src/tree.rb
+++ src/tree.rb
@@ -1,28 +1,8 @@
 require_relative 'plants'
-require 'colorize'
+
 
 class Tree < Plant
   def info
     puts 'This plant is tree'
-  end
-
-  def blossom
-    if @color == 'black'
-      puts @title.black
-    elsif @color == 'red'
-      puts @title.red
-    elsif @color == 'brown'
-      puts @title.brown
-    elsif @color == 'blue'
-      puts @title.blue
-    elsif @color == 'magenta'
-      puts @title.magenta
-    elsif @color == 'cyan'
-      puts @title.cyan
-    elsif @color == 'gray'
-      puts @title.gray
-    else
-      puts @title.green
-    end
   end
 end
